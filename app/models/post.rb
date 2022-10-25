557ac1ad06a10874bdc84bd60215a41669bcef8f
brynex20 <brynespiritu@gmail.com>
2022-10-25 12:33:44 +0800

    added belongs to user in model Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,7 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :post_category_ships, dependent: :restrict_with_exception
   has_many :categories, through: :post_category_ships
+  belongs_to :user
 
   def destroy
     update(delete_at: Time.now)
