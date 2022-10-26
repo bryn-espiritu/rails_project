c635be31836e134676306d0c14e3de2563135a9b
brynex20 <brynespiritu@gmail.com>
2022-10-26 11:58:25 +0800

    modify schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_25_104218) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_124346) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -74,6 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_104218) do
     t.datetime "updated_at", null: false
     t.datetime "delete_at"
     t.integer "user_id"
+    t.integer "comments_count", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
